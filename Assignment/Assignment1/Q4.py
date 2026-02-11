70d9308e503660fbe2de96032e75b138721f35ee
prachi120502 <89413166+prachi120502@users.noreply.github.com>
2026-02-11 14:30:38 +0530

    Delete Assignment/Assignment1 directory

--- Assignment/Assignment1/Q4.py
+++
@@ -1,9 +0,0 @@
-# Program to find average of three numbers
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
-
-average = (a + b + c) / 3
-
-print("Average =", average)
